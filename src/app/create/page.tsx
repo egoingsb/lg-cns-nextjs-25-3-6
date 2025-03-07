e296613b954d54cf780806bd9269a44487b214c3
egoing <egoing@gmail.com>
2025-03-07 15:45:35 +0900

    사용자와 상호작용을 해야 하는 컴포넌트는 client 컴포넌트여야 한다. "use client"를 사용한다. 리디렉션은 useRouter의 push를 사용한다.

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,32 +1,40 @@
-export default async function Create() {
+"use client";
+import { useRouter } from "next/navigation";
+export default function Create() {
+    const router = useRouter();
+    const submitHandler = async (event: React.FormEvent) => {
+        event.preventDefault();
+        const form = event.target as HTMLFormElement;
+        const title = form._title.value;
+        const body = form._body.value;
+        const resp = await fetch(`http://localhost:9999/topics`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title,
+                body,
+            }),
+        });
+        const result = await resp.json();
+        const lastCreatedId = result.id;
+        router.push(`/read/${lastCreatedId}`);
+    };
     return (
         <div>
-            <h1>
-                <a href="/">Web</a>
-            </h1>
-            <ol>
-                <li>
-                    <a href="/read/1">html</a>
-                </li>
-                <li>
-                    <a href="/read/2">css</a>
-                </li>
-                <li>
-                    <a href="/read/3">js</a>
-                </li>
-            </ol>
-            Create
-            <ul>
-                <li>
-                    <a href="/create">create</a>
-                </li>
-                <li>
-                    <a href="/update/id">update</a>
-                </li>
-                <li>
-                    <button>Delete</button>
-                </li>
-            </ul>
+            <h2>Create</h2>
+            <form onSubmit={submitHandler}>
+                <p>
+                    <input type="text" name="_title" placeholder="title" />
+                </p>
+                <p>
+                    <textarea name="_body" placeholder="body"></textarea>
+                </p>
+                <p>
+                    <input type="submit" value="create" />
+                </p>
+            </form>
         </div>
     );
 }
