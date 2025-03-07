975e8e303976ad1ff4ec4356c52e60e31cdece54
egoing <egoing@gmail.com>
2025-03-07 16:31:26 +0900

    선택된 페이지가 있는 경우만 control이 보여지도록 처리

--- src/app/Control.tsx
+++ src/app/Control.tsx
@@ -1,6 +1,12 @@
+"use client";
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 export function Counter() {
+    const params = useParams<{ id: string }>();
+    if (params.id === undefined) {
+        return null;
+    }
     return (
         <>
             <li>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Link from "next/link";
-import { Counter } from "./control";
+import { Counter } from "./Control";
 
 export const metadata: Metadata = {
     title: "Create Next App",
