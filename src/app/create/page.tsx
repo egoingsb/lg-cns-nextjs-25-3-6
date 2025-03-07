f8e75c5f6ad6815995c2733015bcd9239863d3a1
egoing <egoing@gmail.com>
2025-03-07 11:37:46 +0900

    모든 페이지에 동일 레이아웃을 적용

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,3 +1,32 @@
 export default async function Create() {
-    return "Create";
+    return (
+        <div>
+            <h1>
+                <a href="/">Web</a>
+            </h1>
+            <ol>
+                <li>
+                    <a href="/read/1">html</a>
+                </li>
+                <li>
+                    <a href="/read/2">css</a>
+                </li>
+                <li>
+                    <a href="/read/3">js</a>
+                </li>
+            </ol>
+            Create
+            <ul>
+                <li>
+                    <a href="/create">create</a>
+                </li>
+                <li>
+                    <a href="/update/id">update</a>
+                </li>
+                <li>
+                    <button>Delete</button>
+                </li>
+            </ul>
+        </div>
+    );
 }
--- src/app/read/[id]/page.tsx
+++ src/app/read/[id]/page.tsx
@@ -5,5 +5,34 @@ type ReadProps = {
 };
 export default async function Read(props: ReadProps) {
     const params = await props.params;
-    return <>Read! {params.id}</>;
+    return (
+        <div>
+            <h1>
+                <a href="/">Web</a>
+            </h1>
+            <ol>
+                <li>
+                    <a href="/read/1">html</a>
+                </li>
+                <li>
+                    <a href="/read/2">css</a>
+                </li>
+                <li>
+                    <a href="/read/3">js</a>
+                </li>
+            </ol>
+            Read! {params.id}
+            <ul>
+                <li>
+                    <a href="/create">create</a>
+                </li>
+                <li>
+                    <a href="/update/id">update</a>
+                </li>
+                <li>
+                    <button>Delete</button>
+                </li>
+            </ul>
+        </div>
+    );
 }
